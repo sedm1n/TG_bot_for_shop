68958ef31ee3a5725d989c9bf5ff7f1f50b5c4aa
Aleksei Krasnov <no@no.ru>
2024-03-18 19:56:20 +0300

    first upload

---
+++ Readme.md
@@ -0,0 +1,2 @@
+This is my pet project
+Use matreals in Yt channel PythonHubStudio
---
+++ common/bot_cmds_list.py
@@ -0,0 +1,8 @@
+from aiogram.types import BotCommand
+
+private = [
+    BotCommand(command='menu', description='Open menu'),
+    BotCommand(command='payment', description='Payment'),
+    BotCommand(command='about', description='Open about'),
+    BotCommand(command='shipping', description='Shipping'),
+]
